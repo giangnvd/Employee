80bb1874d1ddeded5c5af95f6b2ec51ae1273c52
giangnvd <giangnvd00570@fpt.edu.vn>
2018-11-09 11:23:58 +0700

    Finish

---
+++ Annotation/Context/EmployContext.cs
@@ -0,0 +1,14 @@
+﻿using Annotation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Annotation.Context
+{
+    public class EmployContext : DbContext
+    {
+        public  DbSet<Employee> Employee { get; set; }
+    }
+}
